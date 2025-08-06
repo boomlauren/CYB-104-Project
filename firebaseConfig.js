f6a3b0cf9967bf835f9b7e590441657e18fe7340
boomlauren <boomfrontenddeveloper@gmail.com>
2025-08-06 04:13:47 +0100

    change

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNflT8O3ZA7izsLKOQIEje5H7CXqg2leI",
+  authDomain: "schoolauthapp-86011.firebaseapp.com",
+  projectId: "schoolauthapp-86011",
+  storageBucket: "schoolauthapp-86011.firebasestorage.app",
+  messagingSenderId: "839778884084",
+  appId: "1:839778884084:web:5a4d044947f5c1dc8081a1",
+  measurementId: "G-2X129S3KXJ",
+};
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBNflT8O3ZA7izsLKOQIEje5H7CXqg2leI",
-    authDomain: "schoolauthapp-86011.firebaseapp.com",
-    projectId: "schoolauthapp-86011",
-    storageBucket: "schoolauthapp-86011.firebasestorage.app",
-    messagingSenderId: "839778884084",
-    appId: "1:839778884084:web:5a4d044947f5c1dc8081a1",
-    measurementId: "G-2X129S3KXJ"
-  };
+// Initialize Firebase Authentication and get a reference to the service
+const auth = firebase.auth();
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-
-  export { auth };
+// Export the auth object for use in other files
+export { auth };
